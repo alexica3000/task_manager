c9cefdf06622d38057eb92389b8b86f92db46dfc
Alex S <sanatateacom@gmail.com>
2021-02-04 20:27:34 +0200

    created repository and service

---
+++ src/task/service/TaskService.java
@@ -0,0 +1,44 @@
+package task.service;
+
+import task.domain.Task;
+import task.domain.TaskPriority;
+import task.repository.TaskRepository;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public class TaskService {
+    private final TaskRepository taskRepository;
+
+    public TaskService(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+
+    public List<Task> getByPriority(TaskPriority priority) {
+        return taskRepository.findByPriority(priority);
+    }
+
+    public List<Task> getAll() {
+        return taskRepository.findAll();
+    }
+
+    public List<Task> getForToday() {
+        return taskRepository.findByDate(LocalDate.now());
+    }
+
+    public boolean save(Task task) {
+        return taskRepository.save(task);
+    }
+
+    public boolean update(Task task) {
+        return taskRepository.save(task);
+    }
+
+    public boolean delete(Task task) {
+        return taskRepository.delete(task);
+    }
+
+    public Task getById(String taskId) {
+        return taskRepository.findById(taskId);
+    }
+}
